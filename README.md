c5ee2a1f9165edaa514af0f13613ce4c64d80cf1
Gavin <gavinbrrtt@gmail.com>
2021-07-22 23:58:06 -0700

    Moved variable CSS code to React styled components. Added a README.

---
+++ README.md
@@ -0,0 +1,2 @@
+## Description
+This is a simple image carousel built with React that can take a variable number of images.
--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,26 @@ const StyledLabel = styled.label`
 	}
 `;
 
-// FIXME: pass in number of image order, pass in percentage amount to shift by (order * -100%: -0%, -100%, -200%, -300%...)
-/*
 const Input = styled.input`
-	&:nth-of-type(${prop => prop.num}):checked ~ label:nth-of-type(${prop => prop.num}) { 
+	&:nth-of-type(${prop => prop.num + 1}):checked ~ label:nth-of-type(${prop => prop.num + 1}) { 
 	   z-index: 0;
 	}
-	&:nth-of-type(${prop => prop.num}):checked ~ label {
+	&:nth-of-type(${prop => prop.num + 1}):checked ~ label {
 		transform: translate3d(-${prop => prop.shift}%, 0, 0);
 	}
 `;
 
-const Input = styled.input`
-	&:nth-of-type(${prop => prop.num}):checked ~ label:nth-of-type(${prop => prop.num}) { 
-	   z-index: 0;
-	}
-	&:nth-of-type(${prop => prop.num}):checked ~ label {
-		transform: translate3d(-100%, 0, 0);
-	}
-`;
-*/
-
 const CarouselCore = ({paths}) => {
 	return (<form>
+		{paths ? paths.map((path, index) => <Input key={index} name={"image"} type={"radio"} value={`image${index}`} id={`image${index}`} num={index} shift={100 * index}/>) : ''}
 		{paths ? paths.map((path, index) => {
-			if (index == 0) return <input key={index} name={"image"} type={"radio"} value={`image${index}`} id={`image${index}`} autoFocus={true}/>
-			return <input key={index} name={"image"} type={"radio"} value={`image${index}`} id={`image${index}`}/>
-		}) : ''}
-		{paths ? paths.map((path, index) => {
-			return <StyledLabel path={path} key={index} for={`image${index}`} label_name={`image${index}`} /*htmlFor={`image${index}`}*//>
+			return <StyledLabel path={path} key={index} for={`image${index}`} label_name={`image${index}`}/>
 		}) : ''}
 	</form>);
 }
 
 const App = () => {
-	const arr = [ "assets/boat.jpg", "assets/fuji.jpg", "assets/rocks.jpg", "assets/valley.jpg" ];
+	const arr = [ "assets/boat.jpg", "assets/fuji.jpg", "assets/bilbs.jpg", "assets/rocks.jpg", "assets/valley.jpg" ];
 	return (<div className="container-box">
 		<div className={'container'}>
 			<CarouselCore paths={arr}/>
